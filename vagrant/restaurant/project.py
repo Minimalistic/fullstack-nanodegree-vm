b98848b1e2ea398b935f1c61d46a2d1621de71c4
Jason Marsh <regularly.jason@gmail.com>
2017-04-06 15:20:32 -0500

    Flashing Message Quiz - Completed

--- vagrant/restaurant/project.py
+++ vagrant/restaurant/project.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -54,6 +54,7 @@ def newMenuItem(restaurant_id):
             restaurant_id)
         session.add(newItem)
         session.commit()
+        flash ("New menu item created!")
         return redirect(url_for('restaurantMenu', restaurant_id = 
             restaurant_id))
     else:
@@ -70,6 +71,7 @@ def editMenuItem(restaurant_id, menu_id):
             editedItem.name = request.form['name']
         session.add(editedItem)
         session.commit()
+        flash ("Edited menu item saved!")
         return redirect(url_for('restaurantMenu',
                                 restaurant_id=restaurant_id))
     else: 
@@ -86,6 +88,7 @@ def deleteMenuItem(restaurant_id, menu_id):
     if request.method == 'POST':
         session.delete(itemToDelete)
         session.commit()
+        flash ("Menu item deleted successfully!")
         return redirect(url_for('restaurantMenu',
                                 restaurant_id=restaurant_id))
     else:
@@ -94,6 +97,8 @@ def deleteMenuItem(restaurant_id, menu_id):
                                 restaurant_id=restaurant_id)
 
 if __name__ == '__main__':
+    #This should be a secure password, but is not for learning.
+    app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
 

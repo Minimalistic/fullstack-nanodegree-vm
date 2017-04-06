3cf8032846116d325349c65072f3cab78a019d9c
Jason Marsh <regularly.jason@gmail.com>
2017-04-06 13:59:09 -0500

    Edit Menu Item Form Quiz - Complete

--- vagrant/restaurant/project.py
+++ vagrant/restaurant/project.py
@@ -1,5 +1,6 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -19,7 +20,7 @@ def MainPage():
     output += "<h1>Welcome to Restaurants and Menus</h1>"
     output += "<p>Below you will find the available Restaurants \
                 in the database.</p>"
-    output += "<a href ='/restaurants/1/menu/'>Restaurant 1</a>"
+    output += "<a href ='/restaurant/1/menu/'>Restaurant 1</a>"
     output += "<br>"
     output += "<a href ='/restaurant/2/menu/'>Restaurant 2</a>"
     output += "<br>"
@@ -44,36 +45,38 @@ def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
     return render_template('menu.html', restaurant=restaurant, items=items)
-    # Old method without templating below:
-    # output = ''
-    # output += "<h3>"
-    # output += "<a href='/'>Go Back to Main Page</a>"
-    # output += "</h3>"
-    # for i in items:
-    #     output += i.name
-    #     output += '</br>'
-    #     output += i.price
-    #     output += '</br>'
-    #     output += i.description
-    #     output += '</br>'
-    #     output += "<a href ='/restaurant/<restaurant_id>/menu/<menu_id>/edit/'>Edit</a>"
-    #     output += " / "
-    #     output += "<a href ='/restaurant/<restaurant_id>/menu/<menu_id>/delete/'>Delete</a>"
-    #     output += '</br>'
-    #     output += '</br>'
-    # output += "To add a new item to this menu, "
-    # output += "<a href='#''>click here.</a>"
-    # return output
 
 # Task 1: Create route for newMenuItem function here
-@app.route('/restaurant/<int:restaurant_id>/menu/new/')
+@app.route('/restaurant/<int:restaurant_id>/menu/new/', methods=['GET','POST'])
 def newMenuItem(restaurant_id):
-    return "page to create a new menu item. Task 1 complete!"
+    if request.method == 'POST':
+        newItem = MenuItem(name = request.form['name'],restaurant_id =
+            restaurant_id)
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id = 
+            restaurant_id))
+    else:
+        return render_template('newmenuitem.html', restaurant_id = 
+            restaurant_id)
 
 # Task 2: Create route for editMenuItem function here
-@app.route('/restaurant/<restaurant_id>/menu/<menu_id>/edit/')
+@app.route('/restaurant/<restaurant_id>/menu/<menu_id>/edit/', methods = ['GET',
+                                                                        'POST'])
 def editMenuItem(restaurant_id, menu_id):
-    return "page to edit a menu item. Task 2 complete!"
+    editedItem = session.query(MenuItem).filter_by(id=menu_id).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            editedItem.name = request.form['name']
+        session.add(editedItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu',
+                                restaurant_id=restaurant_id))
+    else: 
+        return render_template('editmenuitem.html',
+                                restaurant_id=restaurant_id,
+                                menu_id=menu_id,
+                                i=editedItem)
 
 # Task 3: Create a route for deleteMenuItem function here
 @app.route('/restaurant/<restaurant_id>/menu/<menu_id>/delete/')

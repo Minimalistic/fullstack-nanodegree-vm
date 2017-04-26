ef2874935ee1351c2da661e162fba4876df29f71
Jason Marsh <regularly.jason@gmail.com>
2017-04-26 10:51:28 -0500

    Feature: Google logout/revoke of login status works

--- vagrant/my_restaurants_and_menus/finalproject.py
+++ vagrant/my_restaurants_and_menus/finalproject.py
@@ -114,12 +114,12 @@ def gconnect():
 def gdisconnect():
     # Only disconnect a connected user.
     credentials = login_session.get('credentials')
+    access_token = credentials
     if credentials is None:
         response = make_response(json.dumps('Current user not connected.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
     # Execute HTTP GET request to revoke token.
-    access_token = credentials.access_token
     url = ('https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token)
     h = httplib2.Http()
     result = h.request(url, 'GET')[0]
